be9a93b4e87d3b8d071f9085c4925d555281859f
iiiivanCh <iiiivanchm@yandex.ru>
2022-06-07 17:28:01 +0300

    work on bugs

--- task01.js
+++ task01.js
@@ -3,62 +3,97 @@
 const tableD = () => {
 
   let a = 1;
-  let str = a + '**' + 'm';
+  let str = ' ' + a + '**' + 'm';
 
-  while (a < 10) {
+  while (a < 11) {
 
     for (let m = 1; m < 11; m++) {
-      let degree = String(a ** m) + ' ';
-      let z = degree.length;
-
-      switch (z) {
-        case 2:
-          degree = ('            ' + degree);
-          break;
-        case 3:
-          degree = ('           ' + degree);
-          break;
-        case 4:
-          degree = ('          ' + degree);
-          break;
-        case 5:
-          degree = ('         ' + degree);
-          break;
-        case 6:
-          degree = ('        ' + degree);
-          break;
-        case 7:
-          degree = ('       ' + degree);
-          break;
-        case 8:
-          degree = ('      ' + degree);
-          break;
-        case 9:
-          degree = ('     ' + degree);
-          break;
-        case 10:
-          degree = ('    ' + degree);
-          break;
-        case 11:
-          degree = ('   ' + degree);
-          break;
-        case 12:
-          degree = ('  ' + degree);
-          break;
-        default:
-          break;
-      }
-
-      str += degree;
+      let degree = ' '.repeat(15) + String(a ** m);
+      let degreeTotal = degree.slice(-14);
+      str += degreeTotal;
     }
 
     console.log(str);
-
     a += 1;
-    str = a + '**' + 'm';
 
+    if (a < 10){
+    str = ' ' + a + '**' + 'm';
+    } else {
+      str = a + '**' + 'm';
+    }
   }
-
 }
 
-tableD();
+tableD()
+
+
+
+
+
+
+
+
+
+
+// const tableD = () => {
+
+//   let a = 1;
+//   let str = a + '**' + 'm';
+
+//   while (a < 10) {
+
+//     for (let m = 1; m < 11; m++) {
+//       let degree = String(a ** m) + ' ';
+//       let z = degree.length;
+
+//       switch (z) {
+//         case 2:
+//           degree = ('            ' + degree);
+//           break;
+//         case 3:
+//           degree = ('           ' + degree);
+//           break;
+//         case 4:
+//           degree = ('          ' + degree);
+//           break;
+//         case 5:
+//           degree = ('         ' + degree);
+//           break;
+//         case 6:
+//           degree = ('        ' + degree);
+//           break;
+//         case 7:
+//           degree = ('       ' + degree);
+//           break;
+//         case 8:
+//           degree = ('      ' + degree);
+//           break;
+//         case 9:
+//           degree = ('     ' + degree);
+//           break;
+//         case 10:
+//           degree = ('    ' + degree);
+//           break;
+//         case 11:
+//           degree = ('   ' + degree);
+//           break;
+//         case 12:
+//           degree = ('  ' + degree);
+//           break;
+//         default:
+//           break;
+//       }
+
+//       str += degree;
+//     }
+
+//     console.log(str);
+
+//     a += 1;
+//     str = a + '**' + 'm';
+
+//   }
+
+// }
+
+// tableD();
--- task02.js
+++ task02.js
@@ -1,16 +1,39 @@
 'use strict'
 
+let verifiedNamber = "4";
+
 const isPrime = (verifiedNamber) => {
+  if (verifiedNamber <= 1) {
+    let mark = false;
+    console.log(mark);
+    return;
+  };
+
+  if (!Number.isFinite(verifiedNamber)) {
+    console.log('Неверные данные, нет решения, требуется предоставить натуральное число');
+    return;
+  };
+
   let mark = true;
-  for (let i = 2; i < Math.sqrt(verifiedNamber); i++) {
-    let m = verifiedNamber % i;
-    if (m === 0) {
+  for (let i = 2; i < Math.sqrt(verifiedNamber) + 1; i++) {
+    let markPassing = verifiedNamber % i;
+    if (markPassing === 0) {
       mark = false;
       break;
     }
   }
   console.log(mark);
-}
+};
 
-isPrime(1987);
-isPrime(1991);
+
+isPrime(29);
+isPrime(-29);
+isPrime(0);
+isPrime(4);
+isPrime(5);
+isPrime(6);
+isPrime(7);
+isPrime(8);
+isPrime(9);
+isPrime(11);
+isPrime("3");
